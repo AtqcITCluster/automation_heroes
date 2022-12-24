fc6c30511b6b483f2cf0e8d9a160b1e1b0062343
Artem Korchevyi <artem.korchevy@gmail.com>
2022-12-24 13:28:15 +0200

    added homework package with class examples

--- src/test/java/homeWorks/akorchevyi/JUnitTestNGHomeWork.java
+++ src/test/java/homeWorks/akorchevyi/JUnitTestNGHomeWork.java
@@ -1,6 +1,6 @@
 package homeWorks.akorchevyi;
 
-public class HandlingExceptionsHomeWork {
+public class JUnitTestNGHomeWork {
 
     // HERE WILL YOUR HOME WORK
     public static void main(String[] args) {
